aa950954397d1f5192b9a62d1498e0d7c5330453
DaniilPetrovsliy <120306367+DaniilPetrovsliy@users.noreply.github.com>
2022-12-23 21:05:40 +0300

    Update bpm_22_2_2.cpp

--- bpm_22_2_2.cpp
+++ bpm_22_2_2.cpp
@@ -1,36 +1,24 @@
-#include <vector>
-#include <map>
-#include <set>
-#include <algorithm>
-#include <cmath>
-#include <queue>
-#include <fstream>
-#define _USE_MATH_DEFINES
+#include <iostream>
 
 int factorial(int x) {
-	if (x == 0) {
-		return 1;
-	}
-	else {
-		int sum = 1;
-		for (int i = 1; i <= x; i++) {
-			sum *= (2 * i - 1);
-		}
-		return sum;
-	}
+    if (x == 0) return 1;
+    else {
+    int sum = 1;
+    for (int i = 1; i <= x; i++) {
+        sum *= (2 * i - 1);
+    }
+    return sum;
+  }
 }
 
 int main() {
-	double x,y;
-	int k1 = 0, k2 = 0;
-	while (std:: cin >> x >> y){
-		if (y >= x && y >= -x && y <= 2-x*x) {
-			k1++;
-		else if (y <= x && y <= -x && y >= 2-x*x) {
-			k2++;
-		}
-	}
-		std::cout << "IN ZONE" << k1 << "\n";
-		std::cout << "Out Zone" << k2 << "\n";
-		std::cout << "pr" << k2 / k1 << "\n";
-
+    double x,y;
+    int k1 = 0, k2 = 0;
+    while (std::cin >> x >> y){
+    if (y >= x && y >= -x && y <= 2-x*x) k1++;
+    else k2++;
+    std::cout << "In zone: " << k1 << "\n";
+    std::cout << "Out zone: " << k2 << "\n";
+    }
+    system("pause");
+}
